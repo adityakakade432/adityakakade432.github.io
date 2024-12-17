e218c813acd98d70c7cc7dadbc4d2e68af70f9f3
adityakakade432 <adikakade432@gmail.com>
2024-12-17 18:30:39 -0500

    fixed mistake in page links

--- src/app/components/NavBar.jsx
+++ src/app/components/NavBar.jsx
@@ -11,26 +11,26 @@ const rubikDirt = Rubik_Dirt({
 const navLinks = [
   {
     title: "About",
-    link: "#about"
+    link: "/#about"
   },
   {
     title: "Work",
-    link: "#work"
+    link: "/#work"
   },
   {
     title: "Projects",
-    link: "#projects"
+    link: "/#projects"
   },
   {
     title: "Contact",
-    link: "#contact"
+    link: "/#contact"
   }
 ]
 const NavBar = () => {
   return (
     <nav className="fixed top-0 left-0 right-0 z-10 bg-[#52796f] bg-op-acity-90">
       <div className="flex flex-wrap items-center justify-between mx-auto px-4 py-2">
-        <Link href={"#landing"} className={`text-2xl md:text-3xl text-[#fefae0] ${rubikDirt.className}`}>aditya &#128187;</Link>
+        <Link href={"/#landing"} className={`text-2xl md:text-3xl text-[#fefae0] ${rubikDirt.className}`}>aditya &#128187;</Link>
         <div className="menu">
           <ul className="flex p-4 md:p-0 md:flex-row md:space-x-8 mt-0">
             {
